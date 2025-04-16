7f6616da45d7d0b30443205f0ee2a4422988fee4
abyanmajid <abyan@abydyl.net>
2025-04-17 03:40:18 +1000

    add: drizzle client and docker compose dev containers

--- database/client.ts
+++ database/client.ts
@@ -1,10 +1,8 @@
+import env from "@/lib/utils/env";
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 
-const queryClient = postgres(process.env.DATABASE_URL, {
-  max: 1,
-  onnotice: () => {}, // This will try to hide weird Drizzle warnings
-});
+const queryClient = postgres(env.DATABASE_URL);
 
 const database = drizzle({ client: queryClient });
 
